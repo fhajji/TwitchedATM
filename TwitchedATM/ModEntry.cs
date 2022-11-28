b104b95202b34b5ec8229774f6e36fa844238a70
Farid Hajji <farid@hajji.name>
2022-11-28 21:41:59 +0100

    SMAPI console commands are only for main farmer

--- TwitchedATM/ModEntry.cs
+++ TwitchedATM/ModEntry.cs
@@ -32,20 +32,30 @@ namespace TwitchedATM
             // react to key presses
             // helper.Events.Input.ButtonPressed += this.OnButtonPressed;
 
-            // cheater SMAPI console command to add to player's money (FED, central bank function, minting money out of thin air)
-            helper.ConsoleCommands.Add("atm_deposit_cheat", $"Add to the player's money.\n\nUsage: atm_deposit_cheat <value> [<sender>]\n- value: the integer amount.\n- sender: name of depositor (default: {CHEATER})", this.CommandDepositCheat);
+            if (Game1.IsMasterGame)
+            {
+                /*
+                 * The following SMAPI console commands are reserved to the main farmer.
+                 * Farm hands aren't allowed to access the ATM
+                 *   - for game-logical reasons (farm hands have no say in finances)
+                 *   - because the account state is NOT replicated to and from the farm hands' computers.
+                 */
 
-            // SMAPI console command to deposit player's money or (simulated) Twitch bits in the account (honest function).
-            helper.ConsoleCommands.Add("atm_deposit", $"Deposit player's money or (simulated) Twitch bits into account.\n\nUsage: atm_deposit <value> [<sender>]\n- value: the integer amount.\n- sender: name of depositor (default: {SELF})", this.CommandDeposit);
+                // cheater SMAPI console command to add to player's money (FED, central bank function, minting money out of thin air)
+                helper.ConsoleCommands.Add("atm_deposit_cheat", $"Add to the player's money.\n\nUsage: atm_deposit_cheat <value> [<sender>]\n- value: the integer amount.\n- sender: name of depositor (default: {CHEATER})", this.CommandDepositCheat);
 
-            // SMAPI console command to withdraw money from account and add it to player's money
-            helper.ConsoleCommands.Add("atm_withdraw", "Move money from account into player's money.", this.CommandWithdraw);
+                // SMAPI console command to deposit player's money or (simulated) Twitch bits in the account (honest function).
+                helper.ConsoleCommands.Add("atm_deposit", $"Deposit player's money or (simulated) Twitch bits into account.\n\nUsage: atm_deposit <value> [<sender>]\n- value: the integer amount.\n- sender: name of depositor (default: {SELF})", this.CommandDeposit);
 
-            // SMAPI console command to display current account activity on the SMAPI console
-            helper.ConsoleCommands.Add("atm_activity_current", "Show current account activity.", this.CommandCurrentActivity);
+                // SMAPI console command to withdraw money from account and add it to player's money
+                helper.ConsoleCommands.Add("atm_withdraw", "Move money from account into player's money.", this.CommandWithdraw);
 
-            // SMAPI console command to display total (summed) account activity on the SMAPI console
-            helper.ConsoleCommands.Add("atm_activity_total", "Show total (summed) account activity.", this.CommandTotalActivity);
+                // SMAPI console command to display current account activity on the SMAPI console
+                helper.ConsoleCommands.Add("atm_activity_current", "Show current account activity.", this.CommandCurrentActivity);
+
+                // SMAPI console command to display total (summed) account activity on the SMAPI console
+                helper.ConsoleCommands.Add("atm_activity_total", "Show total (summed) account activity.", this.CommandTotalActivity);
+            }
         }
 
         /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
